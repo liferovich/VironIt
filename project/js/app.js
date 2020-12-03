4c4cc2870cbcb0ddcf861fd8f9192337a2f6356a
liferovich <56970453+liferovich@users.noreply.github.com>
2020-12-03 14:27:21 +0300

    jk

--- project/js/app.js
+++ project/js/app.js
@@ -1,14 +1,13 @@
 
 const createCalendar = (month, year) => {
 
-    let mon = month - 1; // месяцы в JS идут от 0 до 11
+    let mon = month - 1;
     let date = new Date(year, mon); //1-е число месяца
-    // console.log(d);
 
     let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
 
     // пробелы для первого ряда
-    for (let i = 0; i < getDay(date); i++) {
+    for (let i = 0; i < getDayWeek(date); i++) {
         table += '<td></td>';
     }
 
@@ -16,7 +15,7 @@ const createCalendar = (month, year) => {
     while (date.getMonth() == mon) {
         table += '<td>' + date.getDate() + '</td>';
 
-        if (getDay(date) % 7 == 6) { // вс, последний день - перевод строки
+        if (getDayWeek(date) % 7 == 6) {
             table += '</tr><tr>';
         }
 
@@ -24,8 +23,8 @@ const createCalendar = (month, year) => {
     }
 
     // пробелы для последнего ряда
-    if (getDay(date) != 0) {
-        for (let i = getDay(date); i < 7; i++) {
+    if (getDayWeek(date) != 0) {
+        for (let i = getDayWeek(date); i < 7; i++) {
             table += '<td></td>';
         }
     }
@@ -33,13 +32,13 @@ const createCalendar = (month, year) => {
     // закрыть таблицу
     table += '</tr></table>';
 
-    let elem = document.getElementById("calendar");
-    elem.innerHTML = table;
+    let calendar = document.getElementById("calendar");
+    calendar.innerHTML = table;
 }
 
-function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
+function getDayWeek(date) { // день недели от 0 (пн) до 6 (вс)
     let day = date.getDay();
-    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
+    if (day == 0) day = 7; // воскресенье 
     return day - 1;
 }
 
