5b53295173fcaea3cef3b1502120d0f4ca6bc21d
liferovich <56970453+liferovich@users.noreply.github.com>
2020-12-04 00:16:29 +0300

    try stop server

--- project/server.js
+++ project/server.js
@@ -8,6 +8,7 @@ let server = new http.Server( (req, res) => {
   //POST-запросы типа JSON
   var jsonString = '';
   res.setHeader('Content-Type', 'application/json');
+  res.writeHead(200);
   req.on('data', (data) => { // Пришла информация - записали.
       jsonString += data;
   });
@@ -16,4 +17,6 @@ let server = new http.Server( (req, res) => {
       routing.define(req, res, jsonString);
   });
 });
-server.listen(port, hostname);
+server.listen(port, hostname, () => {
+  console.log(`Server is running on http://${hostname}:${port}`);
+});
